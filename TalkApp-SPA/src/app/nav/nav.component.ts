77af934be6bc72e9ba9e4fec100ba87eab2cdb0d
jonghyun <godaiys@hotmail.com>
2020-08-19 14:00:05 -0400

    Alert with alertifyJS

--- TalkApp-SPA/src/app/nav/nav.component.ts
+++ TalkApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,20 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       (next) => {
-        console.log('log in ok');
+        this.alertify.success('Logged in successfully');
       },
       (error) => {
-        console.log(error);
+        this.alertify.error(error);
       }
     );
   }
@@ -29,5 +33,6 @@ export class NavComponent implements OnInit {
   }
   logout() {
     localStorage.removeItem('token');
+    this.alertify.message('Logged out')
   }
 }
