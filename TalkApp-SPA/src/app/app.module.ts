90cd29770d33268e60c98d302b3e2f83e5cac66e
jonghyun <godaiys@hotmail.com>
2020-09-10 17:43:30 -0400

    Reactive Form validation

--- TalkApp-SPA/src/app/app.module.ts
+++ TalkApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
@@ -51,6 +51,7 @@ export const tokenGetter = () => localStorage.getItem('token');
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
