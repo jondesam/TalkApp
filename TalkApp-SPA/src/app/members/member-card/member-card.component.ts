b68b081f6481c42b9f2515eb0af669e6600fd4f1
jonghyun <godaiys@hotmail.com>
2020-10-01 20:57:55 -0400

    Responsive UI & login to send text and favorite

--- TalkApp-SPA/src/app/members/member-card/member-card.component.ts
+++ TalkApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 import { Rate } from 'src/app/_models/rate';
 import { Skill } from 'src/app/_models/skill';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-member-card',
@@ -13,11 +14,15 @@ import { UserService } from 'src/app/_services/user.service';
 })
 export class MemberCardComponent implements OnInit {
   @Input() user: User;
+  model: any = {};
+  modalRef: BsModalRef;
+  isFavo: boolean = false;
 
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private userService: UserService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private modalService: BsModalService
   ) {}
 
   ngOnInit() {
@@ -37,4 +42,28 @@ export class MemberCardComponent implements OnInit {
       );
   }
 
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  login() {
+    console.log(this.model);
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in successfully');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
+  }
+
+  toggleIsFavo(favo: boolean) {
+    this.isFavo = favo;
+    return this.isFavo;
+  }
 }
