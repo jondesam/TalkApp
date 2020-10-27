07cf7dd5c59524500efc33fe48f5fe3d2b540186
jonghyun <godaiys@hotmail.com>
2020-10-27 18:39:37 -0400

    Clean up app.module & route.ts

--- TalkApp-SPA/src/app/app.module.ts
+++ TalkApp-SPA/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
-import { TimeagoModule } from 'ngx-timeago';
+import { TimeagoModule, TimeagoPipe } from 'ngx-timeago';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { ModalModule } from 'ngx-bootstrap/modal';
@@ -15,7 +15,6 @@ import { RatingModule } from 'ngx-bootstrap/rating';
 import { SidebarModule } from 'ng-sidebar';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { RegisterComponent } from './register/register.component';
@@ -23,52 +22,48 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
-import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
 import { ListsResolver } from './_resolvers/lists.resolver';
-import { MessagesResolver } from './_resolvers/messages.resolver';
-import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { RatesResolver } from './_resolvers/rates.resolver';
 import { TutorBioEditComponent } from './tutor/tutorBioEdit/tutorBioEdit.component';
 import { TutorLanguageEditComponent } from './tutor/tutorLanguageEdit/tutorLanguageEdit.component';
 import { TutorSkillEditComponent } from './tutor/tutorSkillEdit/tutorSkillEdit.component';
 import { TutorEditMenuComponent } from './tutor/_tutorEditMenu/tutorEditMenu.component';
+import { TimeAgoSecondePipe } from './_pipe/timeAgoSeconde.pipe';
+import { MessageboxComponent } from './message/messagebox/messagebox.component';
+import { MessagesboxResolver } from './_resolvers/messagebox.resolver';
+import { TitleCasePipe } from '@angular/common';
 
 export const tokenGetter = () => localStorage.getItem('token');
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
     NavComponent,
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
-    MemberEditComponent,
     PhotoEditorComponent,
-    MemberMessagesComponent,
     TutorBioEditComponent,
     TutorLanguageEditComponent,
     TutorSkillEditComponent,
     TutorEditMenuComponent,
+    TimeAgoSecondePipe,
+    MessageboxComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-
     ReactiveFormsModule,
     BrowserAnimationsModule,
     ButtonsModule.forRoot(),
@@ -96,10 +91,10 @@ export const tokenGetter = () => localStorage.getItem('token');
     MemberDetailResolver,
     MemberListResolver,
     MemberEditResolver,
-    PreventUnsavedChanges,
     ListsResolver,
-    MessagesResolver,
     RatesResolver,
+    MessagesboxResolver,
+    TitleCasePipe,
   ],
   bootstrap: [AppComponent],
 })
--- TalkApp-SPA/src/app/routes.ts
+++ TalkApp-SPA/src/app/routes.ts
@@ -1,22 +1,18 @@
 import { Routes, RunGuardsAndResolvers } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
-import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
-import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+// import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { ListsResolver } from './_resolvers/lists.resolver';
-import { MessagesResolver } from './_resolvers/messages.resolver';
 import { RatesResolver } from './_resolvers/rates.resolver';
-import { TutorBioEditComponent } from './tutor/tutorBioEdit/tutorBioEdit.component';
-import { TutorSkillEditComponent } from './tutor/tutorSkillEdit/tutorSkillEdit.component';
-import { TutorLanguageEditComponent } from './tutor/tutorLanguageEdit/tutorLanguageEdit.component';
 import { TutorEditMenuComponent } from './tutor/_tutorEditMenu/tutorEditMenu.component';
+import { MessageboxComponent } from './message/messagebox/messagebox.component';
+import { MessagesboxResolver } from './_resolvers/messagebox.resolver';
 
 export const appRoutes: Routes = [
   {
@@ -39,39 +35,19 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {
-        path: 'member/edit',
-        component: MemberEditComponent,
-        resolve: { user: MemberEditResolver, rates: RatesResolver },
-        // canDeactivate: [PreventUnsavedChanges],
-      },
-      {
-        path: 'messages',
-        component: MessagesComponent,
-        resolve: { messages: MessagesResolver },
-      },
-      {
         path: 'lists',
         component: ListsComponent,
         resolve: { users: ListsResolver },
       },
       {
-        path: 'tutor/bio/:id',
-        component: TutorBioEditComponent,
-        resolve: { user: MemberEditResolver },
-      },
-      {
-        path: 'tutor/skill/:id',
-        component: TutorSkillEditComponent,
-        resolve: { user: MemberEditResolver },
-      },
-      {
-        path: 'tutor/language/:id',
-        component: TutorLanguageEditComponent,
-      },
-      {
         path: 'tutor/edit/:id',
         component: TutorEditMenuComponent,
         resolve: { user: MemberEditResolver },
+      },
+      {
+        path: 'messagebox/:id',
+        component: MessageboxComponent,
+        resolve: { messages: MessagesboxResolver },
       },
     ],
   },
