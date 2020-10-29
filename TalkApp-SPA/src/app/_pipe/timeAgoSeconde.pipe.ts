0c61c9daf81c4f140d71b1efede9d9a66ff613b4
jonghyun <godaiys@hotmail.com>
2020-10-29 18:42:55 -0400

    TimeAgo remove 'ago'

--- TalkApp-SPA/src/app/_pipe/timeAgoSeconde.pipe.ts
+++ TalkApp-SPA/src/app/_pipe/timeAgoSeconde.pipe.ts
@@ -7,8 +7,10 @@ export class TimeAgoSecondePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     const split = value.split(' ', 2);
     if (+split[0] <= 59 && split[1] === 'seconds') {
-      return 'Less than a minute ago';
+      return '1 min';
     } else {
+      if (split[1] === 'minutes') split[1] = 'min';
+      value = split[0] + ' ' + split[1];
       return value;
     }
   }
